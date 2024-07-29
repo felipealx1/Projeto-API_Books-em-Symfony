db3f73ffade2bf8577e69297acb84d6711d4846d
José Felipe Alexandre Martins <felipealexandre178@gmail.com>
2024-07-29 15:59:39 -0300

    api-book

---
+++ src/Entity/Book.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Entity;
+
+use ApiPlatform\Metadata\ApiResource;
+use App\Repository\BookRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: BookRepository::class)]
+#[ApiResource]
+class Book
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $title = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $author = null;
+
+    #[ORM\Column(nullable: true)]
+    private ?int $edition = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getTitle(): ?string
+    {
+        return $this->title;
+    }
+
+    public function setTitle(string $title): static
+    {
+        $this->title = $title;
+        return $this;
+    }
+
+    public function getAuthor(): ?string
+    {
+        return $this->author;
+    }
+
+    public function setAuthor(string $author): static
+    {
+        $this->author = $author;
+        return $this;
+    }
+
+    public function getEdition(): ?int
+    {
+        return $this->edition;
+    }
+
+    public function setEdition(?int $edition): static
+    {
+        $this->edition = $edition;
+        return $this;
+    }
+}
+
