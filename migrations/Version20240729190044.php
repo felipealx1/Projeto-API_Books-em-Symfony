0cd0ba5090978d3ad0b72c3915d4b43c15df37e1
José Felipe Alexandre Martins <felipealexandre178@gmail.com>
2024-07-29 16:01:13 -0300

    att

--- migrations/Version20240729190044.php
+++ migrations/Version20240729190044.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20240729185548 extends AbstractMigration
+final class Version20240729190044 extends AbstractMigration
 {
     public function getDescription(): string
     {
